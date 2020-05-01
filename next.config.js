5586fdaa54a0881b9a7b0d29bfc117d37f4397dd
Sonjoy Datta <sonjoy.dat@gmail.com>
2020-05-01 20:52:46 +0600

    no message

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 const debug = process.env.NODE_ENV !== 'production'
 
 module.exports = {
+  exportPathMap: async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+    }
+  },
   assetPrefix: !debug ? '/sheba/' : ''
 }
